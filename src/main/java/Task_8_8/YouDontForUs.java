c0b9682bb35b0dfdccbdb9589bf885fcf688c6e2
Elena Kartashova <e.v.kartashova@tinkoff.ru>
2019-10-31 09:08:05 +0300

    новые задачи из 8_8

---
+++ src/main/java/Task_8_8/YouDontForUs.java
@@ -0,0 +1,47 @@
+package Task_8_8;
+
+//Создать множество чисел(Set<Integer>), занести туда 20 различных чисел.
+//Удалить из множества все числа больше 10.
+//
+//Требования:
+//•	Программа не должна выводить текст на экран.
+//•	Программа не должна считывать значения с клавиатуры.
+//•	Класс Solution должен содержать только три метода.
+//•	Метод createSet() должен создавать и возвращать множество Set состоящих из 20 различных чисел.
+//•	Метод removeAllNumbersGreaterThan10() должен удалять из множества все числа больше 10.
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+/*
+Больше 10? Вы нам не подходите
+*/
+public class YouDontForUs {
+    public static Set<Integer> createSet() {
+        // напишите тут ваш код
+        Set<Integer> createSet = new HashSet<Integer>();
+
+        for (int i = 0; i < 20; i++) {
+            createSet.add(i);
+        }
+        return createSet;
+
+    }
+
+    public static Set<Integer> removeAllNumbersGreaterThan10(Set<Integer> set) {
+
+        Iterator<Integer> iterator = set.iterator();
+        while (iterator.hasNext()) {
+            Integer num = iterator.next();
+
+            if (num > 10)
+                iterator.remove();
+        }
+        return set;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(createSet());
+    }
+}
