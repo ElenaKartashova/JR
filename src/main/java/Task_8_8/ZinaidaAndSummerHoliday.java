7ae7e0fbc6d2931d92df076fd1ffde57b837b1c7
elkaZ <elkaZleka@outlook.com>
2019-11-02 14:58:39 +0300

    новая задача 8_4

--- src/main/java/Task_8_8/ZinaidaAndSummerHoliday.java
+++ src/main/java/Task_8_8/ZinaidaAndSummerHoliday.java
@@ -25,14 +25,28 @@ public class ZinaidaAndSummerHoliday {
     public static Map<String, Date> createMap() throws ParseException {
         DateFormat dateFormat = new SimpleDateFormat("MMMMM d yyyy", Locale.ENGLISH);
         Map<String, Date> map = new HashMap<>();
-        map.put("Сталлоне", dateFormat.parse("MAY 1 2012"));
+        map.put("Рената", dateFormat.parse("January 12 2967"));
 
         //напишите тут ваш код
+        map.put("Лидочка", dateFormat.parse("June 21 1950"));
+        map.put("Леночка", dateFormat.parse("February 14 1981"));
+        map.put("Наташа", dateFormat.parse("December 21 1969"));
+        map.put("Нина", dateFormat.parse("April 17 1945"));
+        map.put("Земфира", dateFormat.parse("August 26 1976"));
+        map.put("Мадонна", dateFormat.parse("August 16 1958"));
+        map.put("Майкл", dateFormat.parse("August 29 1958"));
+        map.put("Фреди", dateFormat("September 5 1946"));
+        map.put("Джордж", dateFormat("June 25 1963"));
+        return map;
     }
 
     public static void removeAllSummerPeople(Map<String, Date> map) {
         //напишите тут ваш код
+        for (Map.Entry<String, Data> pair : map.entrySet()) {
+            String key = pair.getKey();
+            Date value = pair.getValue();
 
+        }
     }
 
     public static void main(String[] args) {
