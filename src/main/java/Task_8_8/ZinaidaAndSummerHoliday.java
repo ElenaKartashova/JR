be8a3b4852b5a78a76a228f2c27960475dfc449d
Elena Kartashova <e.v.kartashova@tinkoff.ru>
2019-11-06 18:56:44 +0300

    задача 8_4_

--- src/main/java/Task_8_8/ZinaidaAndSummerHoliday.java
+++ src/main/java/Task_8_8/ZinaidaAndSummerHoliday.java
@@ -11,42 +11,46 @@ package Task_8_8;
 •	Метод removeAllSummerPeople() должен удалять из словаря всех людей, родившихся летом.
  */
 
+import sun.util.calendar.LocalGregorianCalendar;
+
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-import java.util.Map;
-import java.util.HashMap;
+import java.util.*;
 
 /*
 Добрая Зинаида и летние каникулы
 */
 public class ZinaidaAndSummerHoliday {
+
     public static Map<String, Date> createMap() throws ParseException {
         DateFormat dateFormat = new SimpleDateFormat("MMMMM d yyyy", Locale.ENGLISH);
         Map<String, Date> map = new HashMap<String, java.util.Date>();
-        map.put("Рената", dateFormat.parse("January 12 2967"));
+        map.put("Рен", dateFormat.parse("January 12 2967"));
 
         //напишите тут ваш код
-        map.put("Лидочка", dateFormat.parse("June 21 1950"));
-        map.put("Леночка", dateFormat.parse("February 14 1981"));
-        map.put("Наташа", dateFormat.parse("December 21 1969"));
-        map.put("Нина", dateFormat.parse("April 17 1945"));
-        map.put("Земфира", dateFormat.parse("August 26 1976"));
-        map.put("Мадонна", dateFormat.parse("August 16 1958"));
-        map.put("Майкл", dateFormat.parse("August 29 1958"));
-        map.put("Фреди", dateFormat.parse("September 5 1946"));
-        map.put("Джордж", dateFormat.parse("June 25 1963"));
+        map.put("Лид", dateFormat.parse("June 21 1950"));
+        map.put("Лен", dateFormat.parse("February 14 1981"));
+        map.put("Нат", dateFormat.parse("December 21 1969"));
+        map.put("Нин", dateFormat.parse("April 17 1945"));
+        map.put("Зем", dateFormat.parse("August 26 1976"));
+        map.put("Мад", dateFormat.parse("August 16 1958"));
+        map.put("Майк", dateFormat.parse("August 29 1958"));
+        map.put("Фред", dateFormat.parse("September 5 1946"));
+        map.put("Джо", dateFormat.parse("June 25 1963"));
         return map;
+
     }
 
     public static void removeAllSummerPeople(Map<String, Date> map) {
         //напишите тут ваш код
-        for (Map.Entry<String, Date> pair : map.entrySet()) {
-            String key = pair.getKey();
-            Date value = pair.getValue();
 
+        GregorianCalendar calendar = new GregorianCalendar(); // создаем экземпляр календаря
+        for (Iterator<Map.Entry<String, Date>> iterator = map.entrySet().iterator(); iterator.hasNext(); ) {
+            Map.Entry<String, Date> entry = iterator.next();
+            calendar.setTime(entry.getValue()); // записываем дату в календарь
+            if (calendar.get(Calendar.MONTH) > 4 && calendar.get(Calendar.MONTH) < 8) // проверяем месяц
+                iterator.remove();
         }
     }
 
