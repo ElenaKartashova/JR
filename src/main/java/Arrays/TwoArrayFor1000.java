fc0cdcb5543d1d251041344aa2d5f6b0fc7dad90
Elena Kartashova <e.v.kartashova@tinkoff.ru>
2019-12-14 21:28:43 +0300

    массив от 100 до 999

---
+++ src/main/java/Arrays/TwoArrayFor1000.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class TwoArrayFor1000 {
+    public static void main(String[] args) {
+        int[] arrayOne = new int[900];
+        int[] arrayTwo = new int[arrayOne.length];
+
+        int s = 100;
+        for (int i = 0; i < arrayOne.length; i++) {
+            arrayOne[i] = s;
+            s++;
+        }
+        for (int i = 0, j = arrayOne.length-1; i < arrayOne.length; i++, j--) {
+            arrayTwo[j] = arrayOne[i];
+        }
+
+        for (int i = 0; i < arrayOne.length; i++) {
+            System.out.println(arrayTwo[i]);
+        }
+    }
+}
